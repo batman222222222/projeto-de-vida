4cb37e7b1834fd59750f042309d5da23bde5ed4e
batman222222222 <160601863+batman222222222@users.noreply.github.com>
2025-05-06 11:44:54 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -15,7 +15,22 @@ for(let i=0;i <botoes.length;i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo = new Date("2023-10-05T00:00:00");
+const tempoObjetivo = new Date("2025-10-05T00:00:00");
 let temoAtual = new Date();
 
 contadores[0].textContent = tempoObjetivo1 - tempoAtual;
+
+function calculatempo(tempoObjetivo){
+    let tempoAtual = new Date();
+    let tempoFinal = tempoObjetivo - temoAtual;
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
+
+    return dias + " dias " + horas + " horas " + " minutos " +
+    segundos + " segundos ";
+}
